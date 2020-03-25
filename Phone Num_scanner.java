c37b39299c7f220acc13a941d6dbdf127cc68e60
RIDVAN GUNDUZ <58498991+rgunduz17@users.noreply.github.com>
2020-03-24 23:23:50 -0500

    Create Phone Num_scanner.java

---
+++ Phone Num_scanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+ public static void main(String[] args) {
+ 
+ 
+  Scanner scanner = new Scanner(System.in);
+  //System.out.println("Enter areaCode:");
+  int areaCode = scanner.nextInt();
+  //System.out.println("Enter localNumber:");
+  int localNumber = scanner.nextInt();
+  
+  String phoneNumber = "Calling number "+"("+ areaCode +")"+"-"+localNumber;
+  
+  System.out.println(phoneNumber);
+  
+  
+ }
